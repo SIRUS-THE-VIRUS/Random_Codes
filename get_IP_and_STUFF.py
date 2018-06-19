0c715897ebab7719e980233ec5c4729d1e3cda78
SIRUSTHEVIRUS69 <40344706+SIRUSTHEVIRUS69@users.noreply.github.com>
2018-06-19 00:13:51 -0500

    Update get_IP_and_STUFF.py

--- get_IP_and_STUFF.py
+++ get_IP_and_STUFF.py
@@ -42,22 +42,48 @@ def web_url_sections():
 
 
 i = 'yes'
-ping_count = 0
+
+def Local_section(IP):
+    ping_count = 0
+    global alive_list
+    alive_list = []
+    starting_time = time.clock()
+    for m in range(255):
+        exitcode, output = subprocess.getstatusoutput("ping " + str(IP) + str(m) + " -w 1 -n 1")
+        if exitcode == 0:
+            print("System " + IP + str(m) + " is UP !")
+            ping_count += 1
+            alive_list.append(str(IP)+str(m))
+
+    print("Currently there are " + str(ping_count) + " hosts alive")
+    print("Program took " + str(round(time.clock() - starting_time, 1)) + " Seconds to find hosts alive")
+    return
+
+def single_nmap(scan_type,IP):
+    return {
+        1: os.system("nmap " + IP),
+        2: os.system("namp -sV "+ IP),
+        3: os.system("nmap -A "+ IP)
+    }.get(scan_type, print('unexpected Scan Type'))
+
+
+
 while i == 'yes':
     print("******************************************")
     print("Welcome to this Basic scanner")
-    if input("Local or Remote?") == 'Local':
-        # Check for hosts alive on the network
-        IP = input("Enter the network ID eg:(192.168.0.xxx): ")
-        starting_time = time.clock()
-        for m in range(255):
-            exitcode, output = subprocess.getstatusoutput("ping " + str(IP)+ str(m)+" -w 1 -n 1")
-            if exitcode == 0:
-                print("System " + IP + str(m) + " is UP !")
-                ping_count += 1
-        print("Currently there are "+str(ping_count)+" hosts alive")
-        print("Program took "+str(round(time.clock() - starting_time,1))+" Seconds to find hosts alive")
-    else:
+    a = input("Local or Remote?")
+    if a == 'Local':
+        #Check for hosts alive on the network using Ping
+        Local_section(input("Enter the network ID eg:(192.168.0.xxx): "))
+        b = input('What type of scan do you want to perform : (Single/Entire)')
+        #utilizing nmap
+        if b == 'Single':
+            #print(alive_list)
+            IP = input("which IP address would you like to scan")
+            scan_type = input("""1) Find open ports + services
+2) Find open ports + services + Version
+3) Find open ports + services + Version + Run Scripts + OS detection""")
+            single_nmap(scan_type, IP)
+    elif a == 'Remote':
         # incomplete
         web_url_sections()
-
