612c72b8ad1df829e36f3ce0bee2987542aa4e62
SIRUSTHEVIRUS69 <40344706+SIRUSTHEVIRUS69@users.noreply.github.com>
2018-06-20 15:42:06 -0500

    Update get_IP_and_STUFF.py

--- get_IP_and_STUFF.py
+++ get_IP_and_STUFF.py
@@ -1,16 +1,15 @@
-import tld
 import socket
 import os
 import subprocess
 import time
+import tld
 
 
 def get_top_level_domain(url):
     try:
-        name = tld.get_tld(url)
+        name = tld.get_fld(url)
         return name
     except tld.exceptions.TldBadUrl:
-
         return -1
 
 
@@ -21,27 +20,27 @@ def get_IP_Address(url):
 def web_url_sections():
     url = input("Enter the web server url: ")
     domain_name = get_top_level_domain(url)
-
     if get_top_level_domain(url) == -1:
         print("The url is not in the correct format remember https://......")
         print("*******************************************************")
         print("*******************************************************")
-        return url
+        return
     print("******************************************************")
     print("These are the associations(raw) for that Domain name :")
-    for i in range(len(get_IP_Address(domain_name))):
+    for x in range(len(get_IP_Address(domain_name))):
         list1 = get_IP_Address(domain_name)
-        if (len(list1[i])) >= 1 and (list1[i] != domain_name):
-            print(str(list1[i])[1:-1])
+        if (len(list1[x])) >= 1 and (list1[x] != domain_name):
+            print(str(list1[x])[1:-1])
     print("******************************************************")
-    print("\n")
-    if os.system("ping " + domain_name) == 0:
-        print("System is up")
+    exitcode, output = subprocess.getstatusoutput("ping " + domain_name)
+    if exitcode == 0:
+        print("Web Server is alive")
     else:
-        print("System is not responding to ICMP requests")
+        print("Web Server is Down or not responding to ICMP requests")
 
 
 i = 'yes'
+
 
 def Local_section(IP):
     ping_count = 0
@@ -59,13 +58,13 @@ def Local_section(IP):
     print("Program took " + str(round(time.clock() - starting_time, 1)) + " Seconds to find hosts alive")
     return
 
-def single_nmap(scan_type,IP):
+
+def single_nmap(scan_type, IP):
     return {
         1: os.system("nmap " + IP),
-        2: os.system("namp -sV "+ IP),
+        2: os.system("nmap -sV "+ IP),
         3: os.system("nmap -A "+ IP)
     }.get(scan_type, print('unexpected Scan Type'))
-
 
 
 while i == 'yes':
@@ -73,17 +72,18 @@ while i == 'yes':
     print("Welcome to this Basic scanner")
     a = input("Local or Remote?")
     if a == 'Local':
-        #Check for hosts alive on the network using Ping
+        # Check for hosts alive on the network using Ping
         Local_section(input("Enter the network ID eg:(192.168.0.xxx): "))
         b = input('What type of scan do you want to perform : (Single/Entire)')
-        #utilizing nmap
+        # utilizing nmap
         if b == 'Single':
-            #print(alive_list)
+            print(alive_list)
             IP = input("which IP address would you like to scan")
             scan_type = input("""1) Find open ports + services
 2) Find open ports + services + Version
-3) Find open ports + services + Version + Run Scripts + OS detection""")
+3) Find open ports + services + Version + Run Scripts + OS detection\n====>>>>""")
             single_nmap(scan_type, IP)
     elif a == 'Remote':
         # incomplete
         web_url_sections()
+
